94726c78355f835de107fb4b66425cd8078bf1c8
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-05-23 19:20:21 +0330

    Tab context methods completed

--- src/context/TabContextProvider.tsx
+++ src/context/TabContextProvider.tsx
@@ -2,15 +2,34 @@ import { ReactNode, useState } from "react";
 import TabContext from "./TabContext";
 
 export default function TabContextProvider({children} : {children: ReactNode}) {
-    const [tabs, setTabs] = useState([]);
+    const [tabs, setTabs] = useState<any>([]);
 
     const activateTab = (tabId: string) => {
+        setTabs((prev: any) => [...prev.map((tab: any) => {
+            if (tab.id === tabId) {
+                return { ...tab, active: true }
+            }
+            return tab;
+        })]);
     }
+    
     const deactivateTabs = () => {
+        setTabs((prev: any) => [...prev.map((tab: any) => {
+            return { ...tab, active: false }
+        })]);
     }
+
     const addTab = (tab: any) => {
+        setTabs((prev:any) => [...prev, tab]);
     }
+
     const removeTab = (tabId: string) => {
+        setTabs((prev: any) => [...prev.map((tab: any) => {
+            if (tab.id === tabId) {
+                return { ...tab, deleted: true }
+            }
+            return tab;
+        })]);
     }
 
     return (
